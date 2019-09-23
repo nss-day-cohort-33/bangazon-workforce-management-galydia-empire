fabec42d2202dbae81652484b78faa15733b03fc
Samuel Birky <sbirky21@gmail.com>
2019-09-23 16:11:38 -0500

    created models and reran migrations

--- hrapp/models/__init__.py
+++ hrapp/models/__init__.py
@@ -1,3 +1,7 @@
 from .employee import Employee
 from .computer import Computer
 from .employee_computer import EmployeeComputer
+from .department import Department
+from .training_program import TrainingProgram
+from .training_program_employee import TrainingProgramEmployee
+from .modelfactory import model_factory
--- hrapp/models/employee_computer.py
+++ hrapp/models/employee_computer.py
@@ -1,4 +1,8 @@
 from django.db import models
+from .computer import Computer
+from .employee import Employee
+# import datetime
+from django.utils.timezone import now
 
 class EmployeeComputer(models.Model):
     """
@@ -7,5 +11,7 @@ class EmployeeComputer(models.Model):
     methods: none
     """
 
-    employee = models.ForeignKey("Employee", on_delete=models.CASCADE)
-    computer = models.ForeignKey("Computer", on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE)
+    assigned_date = models.DateTimeField(default=now, editable=False)
+    unassigned_date = models.DateTimeField(null=True, blank=True)
