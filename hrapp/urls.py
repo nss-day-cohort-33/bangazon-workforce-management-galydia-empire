cf41cf290d9e14e860cd277329c9aab2b0bbf9c6
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-09-24 15:57:43 -0500

    add working without supervisor status

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import url, include
 from hrapp import views
 from .views import *
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
-    path('employees/', employee_list, name='employee_list'),
+    path('employees/', employee_list, name='employees'),
+    path('employees/form/', employee_form, name='employee_form'),
 ]
--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -1,5 +1,5 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from hrapp.models import Employee
 from django.contrib.auth.decorators import login_required
 from ..connection import Connection
@@ -13,15 +13,16 @@ def employee_list(request):
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
 
-            # TODO: Add to query: e.department,
             db_cursor.execute("""
             select
                 e.id,
                 e.first_name,
                 e.last_name,
                 e.start_date,
-                e.is_supervisor
+                e.is_supervisor,
+                d.dept_name
             from hrapp_employee e
+            join hrapp_department d on e.department_id = d.id
             """)
 
             all_employees = []
@@ -34,13 +35,33 @@ def employee_list(request):
                 employee.last_name = row['last_name']
                 employee.start_date = row['start_date']
                 employee.is_supervisor = row['is_supervisor']
-                # employee.department = row['department']
+                employee.dept_name = row['dept_name']
 
                 all_employees.append(employee)
 
-    template = 'employees/employees_list.html'
-    context = {
-        'employees': all_employees
-    }
+        template = 'employees/employees_list.html'
+        context = {
+            'employees': all_employees
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO hrapp_employee
+            (
+                first_name, last_name, start_date,
+                is_supervisor, department_id
+            )
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (form_data['first_name'], form_data['last_name'],
+                form_data['start_date'], False,
+                form_data["department_id"]))
+
+        return redirect(reverse('hrapp:employees'))
