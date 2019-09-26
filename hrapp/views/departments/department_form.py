816f1faa75ec4ed47adb02ea03cb4d3784dcf55e
scottsilver <scottsilver47@gmail.com>
2019-09-26 09:19:14 -0500

    commit to deal with PR request

--- hrapp/views/departments/department_form.py
+++ hrapp/views/departments/department_form.py
@@ -1,5 +1,6 @@
 import sqlite3
-from django.shortcuts import render, reverse, redirect
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Department
 from ..connection import Connection
